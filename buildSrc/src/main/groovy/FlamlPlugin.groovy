9f07e543a4fd6dc415862d296a17226a6be31a41
Ingmar Steiner <steiner@coli.uni-saarland.de>
2017-03-02 15:20:55 +0100

    apply download task plugin

--- buildSrc/src/main/groovy/FlamlPlugin.groovy
+++ buildSrc/src/main/groovy/FlamlPlugin.groovy
@@ -1,3 +1,5 @@
+import de.undercouch.gradle.tasks.download.DownloadTaskPlugin
+
 import org.gradle.api.*
 import org.gradle.api.plugins.BasePlugin
 
@@ -6,6 +8,7 @@ class FlamlPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
         project.pluginManager.apply(BasePlugin)
+        project.pluginManager.apply(DownloadTaskPlugin)
 
         project.task('text', type: ExtractTextTask)
         project.task('wav', type: ExtractWavTask)
